734ba99b8e677dfff2406bf5d4bf3557fed36eda
Akansh-Verma <58855711+Akansh-Verma@users.noreply.github.com>
2021-05-30 10:05:24 +0530

    Add files via upload

---
+++ Gruntfile.js
@@ -0,0 +1,176 @@
+'use strict';
+
+ const sass = require('node-sass');
+
+ module.exports = function(grunt) {
+
+	
+
+	require('time-grunt')(grunt);
+
+	require('jit-grunt')(grunt, {
+		useminPrepare: 'grunt-usemin',
+        sprite: 'grunt-spritesmith',
+        foo: '@abc/grunt-foo',        // for private modules.
+        bar: 'custom/bar.js'          // for custom tasks.
+    });
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				options: {
+            implementation: sass,
+            sourceMap: true
+                         },
+				files: {
+					'css/styles.css': 'sass/styles.scss'
+				}
+			}
+		},
+		watch: {
+			files: 'sass/*.scss',
+			tasks: ['sass']
+		},
+		browserSync : {
+			dev : {
+				bsFiles:{
+					src: [
+					'sass/*.scss',
+					'*.html',
+					'js/*.js'
+					]
+				},
+				options: {
+					watchTask: true,
+					server: {
+						baseDir: './'
+					}
+				}
+			}
+		},
+		copy: {
+			html: {
+				files: [{
+					expand: true,
+					dot: true,
+					cwd: './',
+					src: ['*.html'],
+					dest: 'dist'
+				}]
+			},
+			fonts: {
+				files: [{
+					expand: true,
+					dot: true,
+					cwd: 'node_modules/font-awesome',
+					src: ['fonts/*.*'],
+					dest: 'dist'
+				}]
+			}
+		},
+		clean: {
+			build: {
+				src: ['dist/']
+			}
+		},
+		imagemin: {
+			dynamic: {
+				files: [{
+					expand: true,
+					dot: true,
+					cwd: './',
+					src: ['img/*.{png,jpg,gif}'],
+					dest: 'dist/'
+				}]
+			}
+		},
+		useminPrepare: {
+			foo: {
+				dest: 'dist',
+				src: ['contactus.html','aboutus.html','index.html']
+			},
+			options: {
+				flow: {
+					steps: {
+						css: ['cssmin'],
+						js: ['uglify']
+					},
+					post: {
+						css: [{
+							name:'cssmin',
+							createConfig: function(context,block) {
+								var generated = context.options.generated;
+								generated.options = {
+									keepSpecialComments: 0, rebase:false
+								}
+							}
+						}]
+					}
+				}
+			}
+		},
+
+		concat: {
+			options: {
+				seperator: ';'
+			},
+			dist: {}
+		},
+		uglify: {
+			dist: {}
+		},
+		cssmin: {
+			dist: {}
+		},
+		filerev: {
+			options: {
+				encoding: 'utf-8',
+				algorithm: 'md5',
+				length:20
+			},
+			release: {
+				files: [{
+					src: [
+						'dist/js/*.js',
+						'dist/css/*.css'
+					]
+				}]
+			}
+		},
+		usemin: {
+			html: ['dist/contactus.html', 'dist/aboutus.html', 'dist/index.html'],
+			options: {
+				assetsDirs: ['dist', 'dist/css', 'dist/js']
+			}
+		},
+		htmlmin: {
+			dist: {
+				options: {
+					collapseWhitespace: true
+				},
+				files: {
+					'dist/index.html': 'dist/index.html',
+					'dist/contactus.html': 'dist/contactus.html',
+					'dist/aboutus.html': 'dist/aboutus.html'
+				}
+			}
+		}
+	});
+   
+	
+	grunt.registerTask('css',['sass']);
+	grunt.registerTask('default',['browserSync', 'watch']);
+	grunt.registerTask('build', [
+		'clean',
+		'copy',
+		'imagemin',
+		'useminPrepare',
+		'concat',
+		'cssmin',
+		'uglify',
+		'filerev',
+		'usemin',
+		'htmlmin'
+		]);
+};
